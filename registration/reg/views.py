40b46bdd64eed9984218ee2515a56f88c31eda3e
KUHHAPTC <55895335+KUHHAPTC@users.noreply.github.com>
2021-12-02 14:16:30 +0300

    Fix/rename (#2)
    * rename serializer class

--- registration/reg/views.py
+++ registration/reg/views.py
@@ -2,13 +2,13 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.serializers import GetAllUserInfo
+from users.serializers import UserDetail
 
 
 class CreateUser(APIView):
 
     def post(self, request):
-        serializer = GetAllUserInfo(data=request.data)
+        serializer = UserDetail(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_201_CREATED, data=serializer.data)
--- registration/users/views.py
+++ registration/users/views.py
@@ -4,14 +4,23 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.serializers import GetAllUserInfo
+from users.serializers import UserDetail
 
 
 class CheckUser(APIView):
 
     def get(self, request):
         users = User.objects.all()
-        serializer = GetAllUserInfo(users, many=True)
+        serializer = UserDetail(users, many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        user = User.objects.get(username=request.user.username)
+        serializer = UserDetail(user)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
 
 
