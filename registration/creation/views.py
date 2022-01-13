182cea22f75b84f7924d4180e93beaee8bb5332d
Eugene <deydara.sama.00@mail.ru>
2022-01-13 14:44:24 +0300

    changed .gitignore

--- registration/creation/views.py
+++ registration/creation/views.py
@@ -3,13 +3,13 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.serializers import UserDetail
+from users.serializers import UserCreate
 
 
 class CreateUser(APIView):
 
     def post(self, request):
-        serializer = UserDetail(data=request.data)
+        serializer = UserCreate(data=request.data)
         serializer.is_valid(raise_exception=True)
         User.objects.create_user(**serializer.data)
         return Response(status=status.HTTP_201_CREATED, data=serializer.data)
