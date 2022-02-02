79f2ee75c45bf593fc9fed83397554ed035cd814
KUHHAPTC <55895335+KUHHAPTC@users.noreply.github.com>
2022-02-03 00:54:08 +0300

    Email verification (#8)
    * str to bool
    
    * changed User model to CustomUser
    
    * added variables for mailing list
    
    * changed registration and added VerifyEmail class

--- registration/creation/views.py
+++ registration/creation/views.py
@@ -1,9 +1,17 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.serializers import UserDetail
+from users.serializers import UserCreate, UserDetail
+from users.models import CustomUser
+
+from registration.settings import EMAIL_HOST_USER
 
 
 class CreateUser(APIView):
@@ -11,5 +19,29 @@ class CreateUser(APIView):
     def post(self, request):
         serializer = UserCreate(data=request.data)
         serializer.is_valid(raise_exception=True)
-        User.objects.create_user(**serializer.data)
-        return Response(status=status.HTTP_201_CREATED, data=serializer.data)
+        user = CustomUser.objects.create_user(**serializer.data)
+        token = default_token_generator.make_token(user)
+        uid64 = urlsafe_base64_encode(force_bytes(user.pk))
+        to_send = f"Link to activate your account: {request.build_absolute_uri()}{uid64}/{token}"
+        mail_subject = 'Activate your account.'
+        send_mail(mail_subject, to_send, EMAIL_HOST_USER, [user.email])
+        return Response(status=status.HTTP_201_CREATED,
+                        data={'info': f'We send activation link on {user.email}'})
+
+
+class VerifyEmail(APIView):
+
+    def get(self, request, uid64, token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uid64))
+            user = get_object_or_404(klass=CustomUser, pk=uid)
+            if not default_token_generator.check_token(user, token):
+                raise ValueError
+        except(TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
+            return Response({'error': 'Activation link is invalid!'})
+
+        user.email_verified = True
+        user.save(update_fields=["email_verified"])
+        return Response(status=status.HTTP_200_OK,
+                        data={'info': f'Successfully verified! Now you can close this tab or '
+                                      f'follow {request.build_absolute_uri("/api/users/me/")}'})
