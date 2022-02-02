8214b33d27a62cb23b9ce4ef5a7458b41b6e82bc
Eugene <deydara.sama.00@mail.ru>
2022-02-03 00:46:55 +0300

    str to bool

---
+++ registration/bool_dict.py
@@ -0,0 +1,12 @@
+from typing import Dict
+
+BOOL_DICT: Dict[str, bool] = {
+    # True
+    "true": True,
+    "True": True,
+    "TRUE": True,
+    # False
+    "false": False,
+    "False": False,
+    "FALSE": False,
+}
